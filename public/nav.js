4280dabd09101245272dbe00458e01997f4d56dc
ZoeJanssen <zjan3871@uni.sydney.edu.au>
2022-05-07 14:10:18 +1000

    nav

--- public/nav.js
+++ public/nav.js
@@ -1,9 +1,9 @@
 /* Toggle between adding and removing the "responsive" class to myMenu when the user clicks on the icon */
 function myFunction() {
     var x = document.getElementById("myMenu");
-    if (x.className === "myMenu") {
+    if (x.className === "menu") {
       x.className += " responsive";
     } else {
-      x.className = "myMenu";
+      x.className = "menu";
     }
   }
