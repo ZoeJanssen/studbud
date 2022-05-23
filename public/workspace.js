a838b8a1e3a4b3bac41c0a11f85d39c028b8010f
ZoeJanssen <zjan3871@uni.sydney.edu.au>
2022-05-23 14:45:43 +1000

    Created changeanle background for workspace

---
+++ public/workspace.js
@@ -0,0 +1,30 @@
+
+// normal / basic change colour style
+function changeStyle(){
+    document.body.style.backgroundColor = "#F0F0F0";
+}
+function changeStyleRed(){
+    document.body.style.backgroundColor = "#f44336";
+}
+function changeStyleOrange(){
+    document.body.style.backgroundColor = "#f6bd60";
+}
+function changeStyleYellow(){
+    document.body.style.backgroundColor = "#fdffb6";
+}
+function changeStyleGreen(){
+    document.body.style.backgroundColor = "#4CAF50";
+}
+function changeStyleBlue(){
+    document.body.style.backgroundColor = "#008CBA";
+}
+function changeStylePurple(){
+    document.body.style.backgroundColor = "#bdb2ff";
+}
+function changeStylePink(){
+    document.body.style.backgroundColor = "#ffc6ff";
+}
+function changeStyleBlack(){
+    document.body.style.backgroundColor = "#495057";
+}
+
